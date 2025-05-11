11cc2470fe742fae7f81189767a6b60d253166a3
neon443 <69979447+neon443@users.noreply.github.com>
2025-05-11 21:34:14 +0100

    initial commit

---
+++ Passy/PassyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassyApp.swift
+//  Passy
+//
+//  Created by neon443 on 11/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PassyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PassyTests/PassyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PassyTests.swift
+//  PassyTests
+//
+//  Created by neon443 on 11/05/2025.
+//
+
+import Testing
+@testable import Passy
+
+struct PassyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
