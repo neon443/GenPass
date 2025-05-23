f91ed86b33cb1bc4795dd74aa4be076203404aed
neon443 <69979447+neon443@users.noreply.github.com>
2025-05-23 20:07:58 +0100

    create app

---
+++ GenPass/GenPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenPassApp.swift
+//  GenPass
+//
+//  Created by neon443 on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct GenPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
